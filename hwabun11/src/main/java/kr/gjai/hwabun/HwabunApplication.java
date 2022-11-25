a6fd0325c1533665e9326544ccab92af1359e0c3
pipi1055 <pipi1055@naver.com>
2022-11-25 15:33:04 +0900

    ggg

---
+++ hwabun11/src/main/java/kr/gjai/hwabun/HwabunApplication.java
@@ -0,0 +1,13 @@
+package kr.gjai.hwabun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwabunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwabunApplication.class, args);
+	}
+
+}
